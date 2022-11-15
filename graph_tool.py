cb66b73de00b647f044522586bd74a9371cd7a24
dlgiovani <gdrosdalima@gmail.com>
2022-11-15 20:02:09 -0300

    added point values

--- graph_tool.py
+++ graph_tool.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 import get_list
+from fractions import Fraction
 
 def graph_default(): # default call for a graph, demonstrative purposoes
-    thisList = get_list.get_ordered_list(True, 20, 4, 1)
+    thisList = get_list.get_ordered_list(True, 20, 0, 1)
 
-    print(thisList)
     plt.plot(thisList[0], thisList[1], 'o--')
+    for i in range(len(thisList[0])):
+        plt.text(thisList[0][i], thisList[1][i]+20, str(thisList[1][i]))
+        plt.text(thisList[0][i], thisList[1][i]+100, Fraction(thisList[1][i], thisList[1][0]))
     plt.show()
