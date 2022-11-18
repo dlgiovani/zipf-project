6e7e7f78fe94e463e1329514d4f21ff439fda908
Giovani Drosda Lima <gdrosdalima@gmail.com>
2022-11-18 18:53:41 -0300

    better graph and console log

--- graph_tool.py
+++ graph_tool.py
@@ -5,8 +5,8 @@ from fractions import Fraction
 def graph_default(): # default call for a graph, demonstrative purposoes
     thisList = get_list.get_ordered_list(True, 20, 0, 1)
 
-    plt.plot(thisList[0], thisList[1], 'o--')
+    plt.plot(thisList[0], thisList[1], '--')
     for i in range(len(thisList[0])):
-        plt.text(thisList[0][i], thisList[1][i]+20, str(thisList[1][i]))
-        plt.text(thisList[0][i], thisList[1][i]+100, Fraction(thisList[1][i], thisList[1][0]))
+        plt.text(thisList[0][i], thisList[1][i], str(thisList[1][i]))
+        print(thisList[0][i], thisList[1][i]+100, Fraction(thisList[1][i], thisList[1][0]), thisList[1][0]/ thisList[1][i])
     plt.show()
